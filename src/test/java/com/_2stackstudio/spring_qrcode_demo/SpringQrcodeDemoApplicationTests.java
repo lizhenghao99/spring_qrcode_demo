1c9864abbf3ff254bfbcc595f9d3bf11d53fb539
Zhenghao Li <lizhenghao99@MacBook-Pro.local>
2021-04-01 20:17:42 -0700

    Initial commit

---
+++ src/test/java/com/_2stackstudio/spring_qrcode_demo/SpringQrcodeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com._2stackstudio.spring_qrcode_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQrcodeDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
